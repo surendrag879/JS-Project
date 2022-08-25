f807bdb358b730c207cec2c7e530da5b5997a91b
surya <surendrag879@gmail.com>
2022-08-25 20:38:51 +0530

    update file

--- assest/js/weather.js
+++ assest/js/weather.js
@@ -1,49 +1,5 @@
 // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 // key : 77a97069fcd0d900a1d2c5abc27f4d38
-// let ResponseData = {
-//     "coord": {
-//         "lon": 75.8333,
-//         "lat": 25.1833
-//     },
-//     "weather": [
-//         {
-//             "id": 804,
-//             "main": "Clouds",
-//             "description": "overcast clouds",
-//             "icon": "04n"
-//         }
-//     ],
-//     "base": "stations",
-//     "main": {
-//         "temp": 299.53,
-//         "feels_like": 299.53,
-//         "temp_min": 299.53,
-//         "temp_max": 299.53,
-//         "pressure": 1005,
-//         "humidity": 80,
-//         "sea_level": 1005,
-//         "grnd_level": 979
-//     },
-//     "visibility": 10000,
-//     "wind": {
-//         "speed": 3.92,
-//         "deg": 227,
-//         "gust": 8.07
-//     },
-//     "clouds": {
-//         "all": 93
-//     },
-//     "dt": 1661353030,
-//     "sys": {
-//         "country": "IN",
-//         "sunrise": 1661301254,
-//         "sunset": 1661347467
-//     },
-//     "timezone": 19800,
-//     "id": 1266049,
-//     "name": "Kota",
-//     "cod": 200
-// }
 
 function getWeather() {
     let cityname = document.getElementById('text').value;
