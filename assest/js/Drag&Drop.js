65f6e237088421f1be5cdaa59f28dea0d2b872df
surya <surendrag879@gmail.com>
2022-09-14 14:29:01 +0530

    update file

--- assest/js/Drag&Drop.js
+++ assest/js/Drag&Drop.js
@@ -4,13 +4,13 @@ function task() {
     let txt_value = document.querySelector('#task_text').value;
     console.log(txt_value);
 
-    let pending = document.querySelector('.pen');
+    let pending = document.querySelector('.taskbox progress-task-list');
     let rejected = document.querySelector('.rej');
     let completed = document.querySelector('.com');
 
 
     if (txt_value == 0) {
-        alert('task not found');
+        alert('Plz enter task');
     }
     else {
         pending.textContent = txt_value;
